dbf75df1202ff055837dda5453c6a358eb426929
khardi <kharchilindanila@gmail.com>
2018-09-22 22:07:08 +0300

    ADD: env merge

--- lang/env.go
+++ lang/env.go
@@ -1,5 +1,10 @@
 package lang
 
+import (
+	"fmt"
+	"sort"
+)
+
 //
 // Env
 //
@@ -11,6 +16,16 @@ func Env() *env {
 	e := new(env)
 	e.state = map[string]Exp{}
 	return e
+}
+
+func (e *env) String() string {
+	s := ""
+	s += fmt.Sprintf("Env:{")
+	for k, v := range e.state {
+		s += fmt.Sprintf("%v:%v; ", k, v.String())
+	}
+	s += "}"
+	return s
 }
 
 func (e *env) Put(name string, exp Exp) {
@@ -26,6 +41,9 @@ func (e *env) Get(name string) Exp {
 }
 
 func (e *env) Equal(other *env) bool {
+	if e.Len() != other.Len() {
+		return false
+	}
 	for k, v := range e.state {
 		ov := other.Get(k)
 		if !ov.Equal(v) {
@@ -33,4 +51,29 @@ func (e *env) Equal(other *env) bool {
 		}
 	}
 	return true
+}
+
+func (e *env) Len() int {
+	return len(e.state)
+}
+
+func (e *env) Keys() []string {
+	s := []string{}
+	for k := range e.state {
+		s = append(s, k)
+	}
+
+	sort.Strings(s)
+	return s
+}
+
+func (e *env) Merge(other *env) *env {
+	result := Env()
+	for _, k := range e.Keys() {
+		result.Put(k, e.Get(k))
+	}
+	for _, k := range other.Keys() {
+		result.Put(k, other.Get(k))
+	}
+	return result
 }
--- lang/env_test.go
+++ lang/env_test.go
@@ -1,9 +1,16 @@
 package lang
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 //
 // Env
+//
+
+//
+// Put Get
 //
 func Test_Env_Put_Get(t *testing.T) {
 	want := Num(5)
@@ -28,6 +35,69 @@ func Test_Env_Get_Nil(t *testing.T) {
 	}
 }
 
+//
+// String
+//
+func Test_Env_String(t *testing.T) {
+	want := `Env:{x:1; }`
+	env := Env()
+	env.Put("x", Num(1))
+
+	got := env.String()
+
+	if got != want {
+		t.Fatalf("\n got : %v \n want: %v", got, want)
+	}
+}
+
+//
+// Len
+//
+func Test_Env_Len_Zero(t *testing.T) {
+	want := 0
+	env := Env()
+
+	got := env.Len()
+
+	if got != want {
+		t.Fatalf("\n got : %v \n want: %v", got, want)
+	}
+}
+
+func Test_Env_Len(t *testing.T) {
+	want := 3
+	env := Env()
+	env.Put("x", Num(1))
+	env.Put("y", Num(2))
+	env.Put("z", Num(2))
+
+	got := env.Len()
+
+	if got != want {
+		t.Fatalf("\n got : %v \n want: %v", got, want)
+	}
+}
+
+//
+// Keys
+//
+func Test_Env_Keys(t *testing.T) {
+	want := []string{"a", "b", "c"}
+	env := Env()
+	env.Put("a", Num(1))
+	env.Put("c", Num(3))
+	env.Put("b", Num(2))
+
+	got := env.Keys()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("\n got : %v \n want: %v", got, want)
+	}
+}
+
+//
+// Equal
+//
 func Test_Env_Equal_True(t *testing.T) {
 	want := true
 
@@ -60,17 +130,54 @@ func Test_Env_Equal_False(t *testing.T) {
 	}
 }
 
-// func Test_Env_Merge(t *testing.T) {
-// 	want := Env()
-// 	want.Put("x", Num(2))
-// 	want.Put("y", Num(3))
+func Test_Env_Equal_False_With_Other_Key(t *testing.T) {
+	want := false
 
-// 	a := Env()
-// 	want.Put("x", Num(1))
+	a := Env()
+	a.Put("x", Num(1))
 
-// 	got := env.Get("x")
+	b := Env()
+	b.Put("y", Num(2))
 
-// 	if !got.Equal(want) {
-// 		t.Fatalf("\n got : %v \n want: %v", got, want)
-// 	}
-// }
+	got := a.Equal(b)
+
+	if got != want {
+		t.Fatalf("\n got : %v \n want: %v", got, want)
+	}
+}
+
+func Test_Env_Equal_False_By_Len(t *testing.T) {
+	want := false
+
+	a := Env()
+	a.Put("x", Num(1))
+
+	b := Env()
+
+	got := a.Equal(b)
+
+	if got != want {
+		t.Fatalf("\n got : %v \n want: %v", got, want)
+	}
+}
+
+//
+// Merge
+//
+func Test_Env_Merge_Add(t *testing.T) {
+	want := Env()
+	want.Put("x", Num(1))
+	want.Put("y", Num(2))
+
+	a := Env()
+	a.Put("x", Num(1))
+
+	b := Env()
+	b.Put("y", Num(2))
+
+	got := a.Merge(b)
+
+	if !got.Equal(want) {
+		t.Fatalf("\n got : %v \n want: %v", got, want)
+	}
+}
